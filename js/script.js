2136ec27fa9cfeb9e4720f987fe5a15ac1f4ec05
krylovkseniya <krylovkseniya39@gmail.com>
2021-12-13 21:34:54 +0400

    создан ToDo List

---
+++ js/script.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const todoControl = document.querySelector(".todo-control");
+const headerInput = document.querySelector(".header-input");
+const todoList = document.querySelector(".todo-list");
+const todoCompleted = document.querySelector(".todo-completed");
+
+let toDoData = [];
+
+
+const render = function () {
+    todoList.innerHTML = '';
+    todoCompleted.innerHTML = '';
+
+    toDoData.forEach (function(item, index) {
+        const li = document.createElement('li');
+        li.classList.add('todo-item');
+        li.innerHTML = '<span class="text-todo">' + item.text + '</span>' + 
+        '<div class="todo-buttons">' +
+        '<button class="todo-remove"></button>' +
+        '<button class="todo-complete"></button>' +
+        '</div>';
+
+        if (item.completed) {
+            todoCompleted.append(li);
+        } else {
+            todoList.append(li);
+        }
+
+        li.querySelector('.todo-complete').addEventListener('click', function() {
+            item.completed = !item.completed;
+            render();
+        });
+
+        li.querySelector('.todo-remove').addEventListener('click', function() {
+            toDoData.splice(index, 1);
+            render();
+        });
+
+    });
+
+    localStorage.setItem("toDoData", JSON.stringify(toDoData));
+
+};
+
+if (JSON.parse(localStorage.getItem('toDoData'))) {
+    toDoData = JSON.parse(localStorage.getItem('toDoData'));
+    render();
+}
+
+todoControl.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const newToDo = {
+        text: headerInput.value,
+        completed: false
+    };
+
+    if (headerInput.value !== '') {
+        toDoData.push(newToDo);
+        headerInput.value = '';
+    }
+
+    render();
+});
